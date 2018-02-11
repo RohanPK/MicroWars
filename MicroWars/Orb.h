2f40dd20ef618912276b6da62548887be2fc02d4
Rohan Prabhu <31131133+RohanPK@users.noreply.github.com>
2018-02-12 01:19:41 +0530

    Update Orb.h

--- MicroWars/Orb.h
+++ MicroWars/Orb.h
@@ -14,17 +14,18 @@ private:
 	float orb_pos_x, orb_pos_y;
 	char orb_colour;
 	int orb_ID;
+	char orb_residual_health_colour;
 	int orb_health;
 	int orb_power;
 	int orb_no_of_units;
 
 public:
-	Orb(float x, float y, char colour, int ID, int health, int no_of_units);
+	Orb(float x, float y, char colour, int ID, int power, int no_of_units);
 	void produce_unit();
-	void change_colour();
 	float return_current_pos(char option);
 	float return_orb_pos(char option);
 	char return_orb_colour();
+	int change_health(char colour);
 	int return_ID();
 	~Orb();
 	
