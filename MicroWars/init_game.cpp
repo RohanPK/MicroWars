b52071eec46ac2b9864b72c3c6cc4599a47e9622
Ishan Sang <f20170069@goa.bits-pilani.ac.in>
2018-05-28 22:46:36 +0530

    Merged branches manually

--- MicroWars/init_game.cpp
+++ MicroWars/init_game.cpp
@@ -5,7 +5,7 @@ void initialise(GameEssentials &G)
 {
 	for(int i = 0; i<G.ORB_COUNT; i++)
 	{
-		G.ORB_VECTOR.push_back(Orb(G.ORB_COORDINATES[i][0],G.ORB_COORDINATES[i][1],G.ORB_RADIUS,G.ORB_COLOUR[i],i,G.ORB_INITIAL_POWER[i],G.ORB_MAX_POWER[i],100));
+		G.ORB_VECTOR.push_back(Orb(G.ORB_COORDINATES[i][0],G.ORB_COORDINATES[i][1],G.ORB_RADIUS,G.ORB_COLOUR[i],G.ORB_INITIAL_POWER[i],G.ORB_MAX_POWER[i],100));
 		for(int j = 0; j<G.ORB_INITIAL_UNITS[i]; j++)
 		{
 			if(G.ORB_VECTOR[i].return_orb_colour()!='X')
@@ -21,17 +21,16 @@ void init_game(RenderWindow &window, bool &start_play)
 {		
 	GameEssentials G =  {
 						G_ORB_COUNT,
-						G_COLOUR_COUNT,
 						30,
 						4,
 						'Y',
-						5,
+						G_PLAYER_COUNT,
 						{'B', 'G', 'Y', 'Y', 'X', 'R', 'R'}, 
 						{{200, 200}, {178, 600}, {400, 250}, {660, 580}, {800, 240}, {260,400},{750,350}}, 
-						{100, 20, 60, 50, 10, 0, 10},
+						{100, 20, 160, 150, 10, 0, 10},
 						{1, 0, 2, 1, 1, 0, 1},
 						{3, 2, 3, 3, 2, 2, 2},
-						{{100, 0, 1}, {20, 0, 0}, {110, 0, 3}}
+						{{100, 0, 1}, {20, 0, 0}, {10, 0, 1}, {310, 0, 3}}
 						};
 
 	G.start_play = start_play;
